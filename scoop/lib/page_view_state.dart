a2bc1dfd8fbeeacf03fca7430859564b1a480ad3
Hayun218 <gkahsdl@icloud.com>
2022-01-01 12:23:57 +0900

    make files

---
+++ scoop/lib/page_view_state.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+enum PageState {
+  dashboard,
+  photos,
+  student,
+  student_info,
+}
