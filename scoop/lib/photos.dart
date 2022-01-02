bddea78288b4db83d3cb8a660e70f55d07ab656f
Hyeongjin Kim <hhjj0506@gmail.com>
2022-01-02 20:40:37 +0900

    photo page & android setup

--- scoop/lib/photos.dart
+++ scoop/lib/photos.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class PhotoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          "사진",
+          style: TextStyle(color: Colors.black, fontSize: 17, fontWeight: FontWeight.w700)),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: List.generate(100, (index) {
+          return Center(
+            child: Text(
+              '사진 $index',
+              style: TextStyle(color: Colors.black, fontSize: 17)),
+            );
+        })
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
