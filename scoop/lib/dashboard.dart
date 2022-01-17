3c30cabef82df7cf2b0584c31ac4a701d963760b
Hyeongjin Kim <hhjj0506@gmail.com>
2022-01-17 12:33:52 +0900

    added calendar

--- scoop/lib/dashboard.dart
+++ scoop/lib/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:scoop/student_info.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -28,17 +29,15 @@ class _DashboardState extends State<DashboardPage> {
         elevation: 0,
       ),
       body: 
-        Text("hello"),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.lightBlueAccent,
-        onPressed: () {
-          Navigator.push(
-            context, 
-            MaterialPageRoute(builder: (context) => const PostAddScreen()),
-          );
-        },
-        child: const Icon(Icons.add),
-      ),
+        Column(
+          children: [
+            TableCalendar(
+              firstDay: DateTime.utc(2022, 1, 1),
+              lastDay: DateTime.utc(2022, 12, 31),
+              focusedDay: DateTime.now(),
+            ),
+          ],
+        ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -66,7 +65,7 @@ class _DashboardState extends State<DashboardPage> {
               onTap: () {
                 Navigator.push(
                   context, 
-                  MaterialPageRoute(builder: (context) => StudentInfoPage()),
+                  MaterialPageRoute(builder: (context) => const StudentInfoPage()),
                 );
               },
             ),
@@ -76,125 +75,3 @@ class _DashboardState extends State<DashboardPage> {
     );
   }
 }
-
-class PostAddScreen extends StatefulWidget {
-  const PostAddScreen({Key? key}) : super(key: key);
-
-  @override
-  _PostAddState createState() => _PostAddState();
-}
-
-class _PostAddState extends State<PostAddScreen>{
-  final formKey = GlobalKey<FormState>();
-
-  String title = '';
-  String category = '';
-  String content = '';
-
-  CollectionReference photo = FirebaseFirestore.instance.collection('photo');
-
-  Future<void> addPhoto() {
-    return photo.add({
-      'title': title,
-    }).then((value) => print("업로드 성공"))
-    .catchError((error) => print("문제가 발생했습니다: $error"));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        iconTheme: const IconThemeData(
-          color: Colors.black,
-        ),
-        title: const Text(
-          "사진 추가",
-          style: TextStyle(color: Colors.black, fontSize: 17, fontWeight: FontWeight.w700)),
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            renderTextFormField(
-              label: '제목', 
-              onSaved: (val) {
-                setState(() {
-                  title = val;
-                });
-              }, 
-              validator: (val) {
-                if (val.length < 1) {
-                  return '제목을 입력해주세요';
-                }
-
-                return null;
-              },
-            ),
-            renderTextFormField(
-              label: '링크', 
-              onSaved: (val) {
-                setState(() {
-                  content = val;
-                });
-              }, 
-              validator: (val) {
-                if (val.length < 1) {
-                  return '링크를 입력해주세요';
-                }
-
-                return null;
-              },
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                if (formKey.currentState!.validate()) {
-                  formKey.currentState!.save();
-                  addPhoto();
-                  Navigator.pop(context);
-                }
-              },
-              style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.lightBlueAccent)),
-              child: const Text(
-                '업로드',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-renderTextFormField({
-    required String label,
-    required FormFieldSetter onSaved,
-    required FormFieldValidator validator,
-  }) {
-
-    return Column(
-      children: [
-        Row(
-          children: [
-            Text(
-              label,
-              style: const TextStyle(
-                fontSize: 12.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-          ],
-        ),
-        TextFormField(
-          onSaved: onSaved,
-          validator: validator,
-        ),
-        Container(height: 16.0),
-      ],
-    );
-  }
