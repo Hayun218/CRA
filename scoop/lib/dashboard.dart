4fc2439e70b12e0d6f999e9fae65f3ab7e9b8a62
Hyeongjin Kim <hhjj0506@gmail.com>
2022-01-23 21:29:39 +0900

    working on calendar

--- scoop/lib/dashboard.dart
+++ scoop/lib/dashboard.dart
@@ -1,7 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:scoop/student_info.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'event.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -11,8 +11,27 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardState extends State<DashboardPage> {
-  final Stream<QuerySnapshot> _photoStream = FirebaseFirestore.instance.collection('photo').snapshots();
-  CollectionReference photo = FirebaseFirestore.instance.collection('photo');
+  Map<DateTime, List<Event>>? selectedEvents;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
+
+  final TextEditingController _eventController = TextEditingController();
+
+  @override
+  void initState() {
+    selectedEvents = {};
+    super.initState();
+  }
+
+  List<Event> _getEventsfromDay(DateTime date) {
+    return selectedEvents?[date] ?? [];
+  }
+
+  @override
+  void dispose() {
+    _eventController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,22 +41,123 @@ class _DashboardState extends State<DashboardPage> {
         iconTheme: const IconThemeData(
           color: Colors.black,
         ),
+        centerTitle: true,
         title: const Text(
           "Dashboard",
           style: TextStyle(color: Colors.black, fontSize: 17, fontWeight: FontWeight.w700)),
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: 
-        Column(
-          children: [
-            TableCalendar(
-              firstDay: DateTime.utc(2022, 1, 1),
-              lastDay: DateTime.utc(2022, 12, 31),
-              focusedDay: DateTime.now(),
+      body: Column(
+        children: [
+          TableCalendar(
+            focusedDay: selectedDay,
+            firstDay: DateTime(DateTime.now().year - 1, DateTime.now().month, DateTime.now().day),
+            lastDay: DateTime(DateTime.now().year + 1, DateTime.now().month, DateTime.now().day),
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            daysOfWeekVisible: true,
+
+            //Day Changed
+            onDaySelected: (DateTime selectDay, DateTime focusDay) {
+              setState(() {
+                selectedDay = selectDay;
+                focusedDay = focusDay;
+              });
+              print(focusedDay);
+            },
+            selectedDayPredicate: (DateTime date) {
+              return isSameDay(selectedDay, date);
+            },
+
+            eventLoader: _getEventsfromDay,
+
+            //To style the Calendar
+            calendarStyle: CalendarStyle(
+              isTodayHighlighted: true,
+              selectedDecoration: BoxDecoration(
+                color: Colors.lightBlueAccent,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              selectedTextStyle: TextStyle(color: Colors.white),
+              todayDecoration: BoxDecoration(
+                color: Colors.grey[300],
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              defaultDecoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              weekendDecoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
             ),
-          ],
+            headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+              formatButtonShowsNext: false,
+              formatButtonDecoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              formatButtonTextStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          ..._getEventsfromDay(selectedDay).map(
+            (Event event) => ListTile(
+              title: Text(
+                event.title,
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () => showDialog(
+          context: context, 
+          builder: (context) => AlertDialog(
+            title: Text("Add Event"),
+            content: TextFormField(
+              controller: _eventController,
+            ),
+            actions: [
+              TextButton(
+                child: Text("Cancel"),
+                onPressed: () => Navigator.pop(context),
+              ),
+              TextButton(
+                child: Text("Ok"),
+                onPressed: () {
+                  if (_eventController.text.isEmpty) {
+
+                  } else {
+                    if (selectedEvents?[selectedDay] != null) {
+                      selectedEvents?[selectedDay]?.add(
+                        Event(title: _eventController.text),
+                      );
+                    } else {
+                      selectedEvents?[selectedDay] = [
+                        Event(title: _eventController.text)
+                      ];
+                    }
+
+                  }
+                  Navigator.pop(context);
+                  _eventController.clear();
+                  setState((){});
+                  return;
+                },
+              ),
+            ],
+          ),
         ),
+      child: const Icon(Icons.add),
+      ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
--- scoop/lib/student_info.dart
+++ scoop/lib/student_info.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:scoop/screens/StudentAddScreen.dart';
 
 
 class StudentInfoPage extends StatefulWidget {
@@ -122,145 +123,7 @@ class _StudentInfoState extends State<StudentInfoPage> {
   }
 }
 
-class StudentAddScreen extends StatefulWidget {
-  const StudentAddScreen({Key? key}) : super(key: key);
 
-  @override
-  _StudentAddState createState() => _StudentAddState();
-}
-
-class _StudentAddState extends State<StudentAddScreen>{
-  final formKey = GlobalKey<FormState>();
-  CollectionReference studentInfo = FirebaseFirestore.instance.collection('HILS');
-
-  Future<void> addStudent() async {
-    return studentInfo.add({
-      'name': name,
-      'birth': birth,
-      'coach': coach,
-      'status': 0,
-    }).then((value) => print("업로드 성공"))
-    .catchError((error) => print("문제가 발생했습니다: $error"));
-  }
-
-  String name = '';
-  String birth = '';
-  String coach = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        iconTheme: const IconThemeData(
-          color: Colors.black,
-        ),
-        title: const Text(
-          "사진 추가",
-          style: TextStyle(color: Colors.black, fontSize: 17, fontWeight: FontWeight.w700)),
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            renderTextFormField(
-              label: '학생 이름', 
-              onSaved: (val) {
-                setState(() {
-                  name = val;
-                });
-              }, 
-              validator: (val) {
-                if (val.length < 1) {
-                  return '이름을 입력해주세요';
-                }
-
-                return null;
-              },
-            ),
-            renderTextFormField(
-              label: '생년월일 (YY/MM/DD)', 
-              onSaved: (val) {
-                setState(() {
-                  birth = val;
-                });
-              }, 
-              validator: (val) {
-                if (val.length < 1) {
-                  return '생년월일을 입력해주세요';
-                }
-
-                return null;
-              },
-            ),
-            // 여기는 select로 바꿔도 될듯
-            renderTextFormField(
-              label: '담당 선생님 이름', 
-              onSaved: (val) {
-                setState(() {
-                  coach = val;
-                });
-              }, 
-              validator: (val) {
-                if (val.length < 1) {
-                  return '담당 선생님을 입력해주세요';
-                }
-
-                return null;
-              },
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                if (formKey.currentState!.validate()) {
-                  formKey.currentState!.save();
-                  addStudent();
-                  Navigator.pop(context);
-                }
-              },
-              style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.lightBlueAccent)),
-              child: const Text(
-                '업로드',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-renderTextFormField({
-    required String label,
-    required FormFieldSetter onSaved,
-    required FormFieldValidator validator,
-  }) {
-
-    return Column(
-      children: [
-        Row(
-          children: [
-            Text(
-              label,
-              style: const TextStyle(
-                fontSize: 12.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-          ],
-        ),
-        TextFormField(
-          onSaved: onSaved,
-          validator: validator,
-        ),
-        Container(height: 16.0),
-      ],
-    );
-  }
 
 listBox ({
   required String text,
@@ -277,3 +140,4 @@ listBox ({
     child: Text(text),
   );
 }
+
