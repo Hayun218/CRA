8c2a3546f1947340e0b17441755cc4262f4ff6be
Hyeongjin Kim <hhjj0506@gmail.com>
2022-02-07 21:27:47 +0900

    working on uploading file

--- scoop/lib/screens/PostAddScreen.dart
+++ scoop/lib/screens/PostAddScreen.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
+import 'package:path/path.dart' as Path;
+import 'package:scoop/api/file_api.dart';
 
 class PostAddScreen extends StatefulWidget {
   const PostAddScreen({Key? key}) : super(key: key);
@@ -13,24 +19,45 @@ class _PostAddState extends State<PostAddScreen>{
   final formKey = GlobalKey<FormState>();
   CollectionReference studentInfo = FirebaseFirestore.instance.collection('notice');
 
-  Future<void> addPost() async {
+  Future<void> addPost(String urlDownload) async {
     return studentInfo.add({
       'title': title, //포스트 제목
       'date': DateFormat.yMd().format(DateTime.now()).toString(), //생성 시간 
       'author': coach, //현재 유저 (user?.uid)
       'content': content, //포스트 내용
       'category': category, //카테고리 
+      'link': urlDownload,
     }).then((value) => print("업로드 성공"))
     .catchError((error) => print("문제가 발생했습니다: $error"));
+  }
+
+  Future uploadFile() async {
+    if (file == null) return;
+
+    final fileName = Path.basename(file!.path);
+    final destination = 'files/$fileName';
+
+    task = FileApi.uploadFile(destination, file!);
+
+    if (task == null) return;
+
+    final snapshot = await task!.whenComplete(() {});
+    final urlDownload = await snapshot.ref.getDownloadURL();
+
+    addPost(urlDownload);
   }
 
   String title = '';
   String coach = '';
   String content = '';
   String category = '공지';
+  File? file;
+  UploadTask? task;
 
   @override
   Widget build(BuildContext context) {
+    final fileName = file != null ? Path.basename(file!.path) : '파일 없음';
+
     return 
     Scaffold(
       resizeToAvoidBottomInset: false,
@@ -113,12 +140,36 @@ class _PostAddState extends State<PostAddScreen>{
                   );
                 }).toList(),
             ),
+            const Text(
+              '파일 선택',
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.lightBlueAccent)),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Icon(Icons.attach_file),
+                  Text(
+                    '파일 선택'
+                  ),
+                ],
+              ),
+              onPressed: () {
+                selectFile();
+              },
+            ),
+            const SizedBox(height: 8,),
+            Text(fileName, style: const TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold)),
             Container(height: 30.0,),
             ElevatedButton(
               onPressed: () async {
                 if (formKey.currentState!.validate()) {
                   formKey.currentState!.save();
-                  addPost();
+                  uploadFile();
                   Navigator.pop(context);
                 }
               },
@@ -135,6 +186,16 @@ class _PostAddState extends State<PostAddScreen>{
       ),
     );
   }
+
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+
+    if (result == null) return;
+    final path = result.files.single.path!;
+
+    setState(() => file = File(path));
+  }
+
 }
 
 renderPostAddField({
